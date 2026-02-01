9acd7c89f2351cdc8e3219ce6344f2932b720a40
mammadovs <12747@holbertonstudents.com>
2026-02-01 17:40:41 +0000

    4. Is leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if current node is leaf
+ * @node: current node
+ * Return: returns 1 if node is leaf
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	return (0);
+}
